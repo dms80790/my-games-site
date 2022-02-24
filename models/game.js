5d8c6902f9e301445f06288727518d1fe0ca7846
David <dms80790@gmail.com>
2022-02-24 07:03:52 -0500

    set up db models

---
+++ models/game.js
@@ -0,0 +1,13 @@
+const mongoose = require(mongoose);
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema({
+  title: {type: String, required: true, maxLength: 100},
+  description: {type: String, required: true},
+  genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}],
+  release_date: {type: Date, required: true, default: Date.now},
+  publisher: {type: Schema.Types.ObjectId, ref: 'Publisher'},
+  platform: {type: Schema.Types.ObjectId, ref: 'Platform'}
+});
+
+module.exports = mongoose.model(GameSchema);
---
+++ models/platform.js
@@ -0,0 +1,8 @@
+const mongoose = require(mongoose);
+const Schema = mongoose.Schema;
+
+const PlatformSchema = new Schema({
+  name: {type: String, required: true, maxLength: 100},
+});
+
+module.exports = mongoose.model(PlatformSchema);

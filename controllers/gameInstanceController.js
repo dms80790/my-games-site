f190f19ff46e398f45278b57c07796a934c82a75
David <dms80790@gmail.com>
2022-02-28 05:41:21 -0500

    implemented get_gameinstance_list and its view

--- controllers/gameInstanceController.js
+++ controllers/gameInstanceController.js
@@ -1,6 +1,11 @@
+const GameInstance = require('../models/gameinstance');
+
 //gameinstance routes
 exports.get_gameinstance_list = function(req, res, next){
-  res.send('not implemented yet.');
+  GameInstance.find({}, function(err, gameinstances){
+    if(err){ return next(err); }
+    res.render('gameinstance_list', {title: 'Game Instances', gameinstance_list: gameinstances});
+  });
 }
 
 exports.get_gameinstance = function(req, res, next){
--- models/gameinstance.js
+++ models/gameinstance.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const {DateTime} = require('luxon');
 
 const GameInstanceSchema = new Schema({
   game: {type: Schema.Types.ObjectId, ref: 'Game'},
   isbn: {type: String, required: true, maxLength: 100},
-  availability: {type: String, required: true, enum:['unreleased', 'available', 'loaned', 'reserved']},
+  status: {type: String, required: true, enum:['unreleased', 'available', 'loaned', 'reserved']},
   due_date: {type: Date, required: true, default: Date.now}
 });
 
 GameInstanceSchema.virtual('url').get(function(){
   return '/catalog/gameinstance/' + this._id;
 });
+
+GameInstanceSchema.virtual('due_date_formatted').get(function(){
+  return DateTime.fromJSDate(this.due_date).toLocaleString(DateTime.DATE_MED);
+})
 
 module.exports = mongoose.model('Game Instance', GameInstanceSchema);
